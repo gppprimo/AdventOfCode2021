f58b429c71b0f1b105e8cc1ac835f00e05343d8e
gppprimo <gpp.primo@gmail.com>
2021-12-02 10:06:47 +0100

    day2

--- Main/main.go
+++ Main/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	day1 "AoC-2021/Day1"
+	day2 "AoC-2021/Day2"
+	utility "AoC-2021/Utility"
 	"fmt"
 	"log"
 	"os"
-	"AoC-2021/Utility"
-	"AoC-2021/Day1"
 )
 
 func main() {
@@ -15,9 +16,19 @@ func main() {
 	}
 
 	// DAY 1
-	input := utility.ReadNumbers(path + "/Day1/" + "input1.txt")
-	fmt.Printf("Puzzle 1.1: %d\n", day1.Puzzle1(input))
-	fmt.Printf("Puzzle 1.2: %d\n", day1.Puzzle2(input))
+	fmt.Println("--- Day 1 ---")
+	inputInt := utility.ReadNumbers(path + "/Day1/" + "input1.1.txt")
+	fmt.Printf("Puzzle 1.1: %d\n", day1.Puzzle1_1(inputInt))
+	fmt.Printf("Puzzle 1.2: %d\n", day1.Puzzle1_2(inputInt))
 
 	// DAY 2
+	fmt.Println("\n--- Day 2 ---")
+	inputString := utility.ReadStrings(path + "/Day2/" + "input2.1.txt")
+	inputActionValue := utility.GetActionValue(inputString)
+	fmt.Printf("Puzzle 2.1: %d\n", day2.Puzzle2_1(inputActionValue))
+	fmt.Printf("Puzzle 2.2: %d\n", day2.Puzzle2_2(inputActionValue))
+
+	// DAY 3
+	fmt.Println("\n--- Day 3 ---")
+
 }
